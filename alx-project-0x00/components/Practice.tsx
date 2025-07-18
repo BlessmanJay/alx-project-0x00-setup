c262d046d7b9cd809dbee12ddf96d054716c6a36
BlessmanJay <blessmanjay@gmail.com>
2025-07-18 08:12:00 +0100

    Add Typing Functional Components

---
+++ alx-project-0x00/components/Practice.tsx
@@ -0,0 +1,15 @@
+/*
+type Props = {
+  name: string;
+  age: string;
+};
+
+const UserCard: React.FC<Props> = ({ name, age }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+    </div>
+  );
+};
+*/
